b96438854a0841cd4ad983b7db2defa5506963c2
Kostian <gor.kosty@gmail.com>
2020-03-27 22:42:47 +0200

    я тут что-то поделал, но ничего полезного

--- Main_window.py
+++ Main_window.py
@@ -5,7 +5,16 @@ screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 background_image = pygame.image.load('images/background.jpeg')
 
-while True:
+run = True
+while run:
     screen.blit(background_image, (0, 0))
     pygame.display.update()
     clock.tick(60)
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+
+pygame.quite()
