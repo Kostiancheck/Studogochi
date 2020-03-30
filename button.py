538370bcdc91896c252bb4411fdc26f41d719a11
Wipersee <kesterera@gmail.com>
2020-03-30 18:44:48 +0300

    created abstract class for Buttons

--- button.py
+++ button.py
@@ -1,21 +1,31 @@
 import pygame
-
 from game_object import GameObject
+from abc import ABC,abstractmethod
+
+class AButton(GameObject,ABC):
+  def __init__(self,x,y,radius,color):
+    GameObject.__init__(self,x,y,radius * 2,radius * 2)
+    self.x = x
+    self.y = y
+    self.radius = radius
+    self.color = color
+
+  
+  def draw(self,surface):
+     pygame.draw.circle(surface, self.color, (self.x,self.y),self.radius)
+
+  @abstractmethod
+  def increase(self,attribute):
+    pass
 
 
-class Ball(GameObject):
-    def __init__(self, x, y, r, color, speed):
-        GameObject.__init__(self,
-                            x,
-                            y,
-                            r * 2,
-                            r * 2)
-        self.radius = r
-        self.diameter = r * 2
-        self.color = color
+class ButtonHealth(AButton):
+  def __init__(self,x,y,radius,color):
+    AButton.__init__(self,x,y,radius,color)
 
-    def draw(self, surface):
-        pygame.draw.circle(surface,
-                           self.color,
-                           self.center,
-                           self.radius)
+  def increase(self,attribute):
+    pass
+
+
+
+
