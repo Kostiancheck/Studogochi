8c6bf94e5b7cd4e93a64bfc4077d4f081385d000
Wipersee <kesterera@gmail.com>
2020-05-08 19:42:03 +0300

    v

--- game_object.py
+++ game_object.py
@@ -2,9 +2,8 @@ from pygame.rect import Rect
 
 
 class GameObject:
-    def __init__(self, x, y, w, h, speed=(0,0)):
+    def __init__(self, x, y, w, h):
         self.bounds = Rect(x, y, w, h)
-        self.speed = speed
 
     @property
     def left(self):
@@ -42,15 +41,3 @@ class GameObject:
     def centery(self):
         return self.bounds.centery
 
-    # def draw(self, surface):
-    #     pass
-
-    def move(self, dx, dy):
-        self.bounds = self.bounds.move(dx, dy)
-
-    def update(self):
-        """"""
-        if self.speed == [0, 0]:
-            return
-
-        self.move(*self.speed)
